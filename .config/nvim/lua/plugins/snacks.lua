ad9b9e31c9e36f197b9c2638c49d8eedc6cfc112
Mudien-UB <mudienub@gmail.com>
2025-11-16 15:47:11 +0700

    fix(nvim/snacks-explorer): fix snacks terminal

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -27,5 +27,44 @@ return {
         gitignored = false,
       },
     },
+    terminal = {
+      snacks_win_opts = {
+        position = "float",
+        width = 0.8,
+        height = 0.5,
+        border = "rounded",
+        keys = {
+          hide = {
+            "<Esc>",
+            function(term)
+              term:hide()
+            end,
+            mode = "t",
+            desc = "Hide terminal",
+          },
+          close = { "q", "close", mode = "n", desc = "Close terminal" },
+        },
+      },
+    },
+  },
+  keys = {
+    {
+      "<leader>T",
+      function()
+        local cwd = vim.fn.getcwd()
+        local cmd = { "pwsh", "-NoLogo", "-Command", "Set-Location -LiteralPath '" .. cwd .. "'; pwsh" }
+
+        require("snacks").terminal(cmd, {
+          win = {
+            position = "float",
+            width = 0.8,
+            height = 0.5,
+            border = "rounded",
+          },
+        })
+      end,
+      desc = "open snacks terminal",
+      mode = { "n", "t" },
+    },
   },
 }
